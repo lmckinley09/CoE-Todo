60876aaa0da92f1632b6ffa4156929afc333c909
Lorna McK <lmckinley@foundationmedicine.com>
2022-12-09 14:31:17 +0000

    fix sql

--- migrations/base/V1.1__data_seed.sql
+++ migrations/base/V1.1__data_seed.sql
@@ -1,16 +1,18 @@
+DO $$
+
+DECLARE USER_BOARD_ID integer;
+DECLARE USER_ID integer;
+DECLARE BOARD_ID integer;
+
+BEGIN
+
 INSERT INTO app_user
 (email_address, first_name, last_name, password, date_of_birth, created, last_modified)
 VALUES('lorna@lorna.com', 'Lorna', 'Mckinley', 'password', '1996-09-26', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
-INSERT INTO app_user
-(email_address, first_name, last_name, password, date_of_birth, created, last_modified)
-VALUES('jane@jane.com', 'Jane', 'Mckinley', 'password', '1986-011-26', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 INSERT INTO board
 ("name", created, last_modified)
-VALUES('Lorna''s Board', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
-INSERT INTO board
-("name", created, last_modified)
-VALUES('Jane''s Board', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+VALUES('Lorna''s Board', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP) RETURNING id INTO BOARD_ID;
 
 INSERT INTO type
 (description)
@@ -25,17 +27,16 @@ VALUES('editer');
 INSERT INTO user_board
 (user_id, board_id)
 VALUES(
-  (select id from app_user where email_address = 'lorna@lorna.com'),
-  (select id from board where name = 'Lorna''s Board')
- );
+(select id from app_user where email_address = 'lorna@lorna.com'),
+(select id from board where name = 'Lorna''s Board')
+) RETURNING id INTO USER_BOARD_ID;
 
- --  INSERT INTO user_board_type
- -- (user_board_id, type_id)
- -- VALUES(
- -- (select id from user_board where ), //not sure how to get
- -- (select id from type where description = 'owner')
- -- );
-
+INSERT INTO user_board_type
+(user_board_id, type_id)
+VALUES(
+USER_BOARD_ID,
+1
+);
 
  INSERT INTO job
 (board_id, title, description, status, completion_date, created, last_modified)
@@ -48,3 +49,5 @@ VALUES(
  VALUES(
    (select id from job where title = 'Lorna''s First Task')
  );
+
+ END $$;
