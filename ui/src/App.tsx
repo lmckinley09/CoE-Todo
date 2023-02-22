dff07c61db687efa6f371417edd038ae454a1f7d
Lorna McK <lmckinley@foundationmedicine.com>
2023-02-22 12:03:07 +0000

    add linting

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 
 function App() {
-  return <div className="App">New App</div>;
+	return <div className="App">New App</div>;
 }
 
 export default App;
