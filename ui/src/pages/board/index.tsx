af6d73c567f6397ed1620791526183ae90760800
Lorna McK <lmckinley@foundationmedicine.com>
2023-02-25 15:21:07 +0000

    set up board page

--- ui/src/pages/board/index.tsx
+++ ui/src/pages/board/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import { Alert } from '@mui/material';
+import { Alert, CssBaseline, Grid, Typography } from '@mui/material';
+import { BoardActions, FocusArea, StyledBox } from './styled';
 import useGetBoard from '@hooks/integrationHooks/useGetBoard';
 
 const Board = (): JSX.Element => {
@@ -11,9 +12,39 @@ const Board = (): JSX.Element => {
 	if (board && board.data) {
 		const { name } = board.data;
 		return (
-			<>
-				<h1>{name}</h1>
-			</>
+			<Grid container component="main" sx={{ height: '100vh' }}>
+				<CssBaseline />
+				<Grid container spacing={1}>
+					<Grid item xs={12} sm={6} md={4}>
+						<BoardActions>
+							<Typography variant="h4">{name}</Typography>
+						</BoardActions>
+					</Grid>
+					<Grid item xs={12} sm={6} md={8}>
+						<FocusArea>
+							<Typography variant="h4">Next Task</Typography>
+						</FocusArea>
+					</Grid>
+				</Grid>
+
+				<Grid container spacing={2}>
+					<Grid item xs={12} sm={4} md={4}>
+						<StyledBox>
+							<Typography variant="h4">Quick Ticks</Typography>
+						</StyledBox>
+					</Grid>
+					<Grid item xs={12} sm={4} md={4}>
+						<StyledBox>
+							<Typography variant="h4">Tasks</Typography>
+						</StyledBox>
+					</Grid>
+					<Grid item xs={12} sm={4} md={4}>
+						<StyledBox>
+							<Typography variant="h4">Projects</Typography>
+						</StyledBox>
+					</Grid>
+				</Grid>
+			</Grid>
 		);
 	}
 	return <Alert severity="error">Error Loading Board Details</Alert>;
--- ui/src/pages/board/styled.ts
+++ ui/src/pages/board/styled.ts
@@ -1,36 +1,31 @@
 import styled from 'styled-components';
-import {
-	Avatar,
-	Alert,
-	Box,
-	Button,
-	CssBaseline,
-	Grid,
-	Paper,
-	TextField,
-	Typography,
-} from '@mui/material';
+import { Box } from '@mui/material';
 
-export const StyledBox = styled(Box)`
+export const BoardActions = styled(Box)`
+	display: flex;
+	align-items: center;
 	margin-top: 20px;
-	margin-left: 20px;
-	margin-right: 20px;
-	min-height: 75vh;
+	min-height: 100px;
+	padding: 10px;
+`;
+
+export const FocusArea = styled(Box)`
+	display: flex;
+	align-items: center;
+	background-color: #f4cfb7;
+	margin-top: 20px;
+	min-height: 100px;
 	padding: 10px;
 	border-radius: 5px;
-	background-color: #f4cfb7;
 	box-shadow: 0px 2px 1px -1px rgb(0 0 0 / 20%), 0px 1px 1px 0px rgb(0 0 0 / 14%),
 		0px 1px 3px 0px rgb(0 0 0 / 12%);
 `;
 
-export const BoardBox = styled(Box)`
-	margin-top: 20px;
-	margin-left: 5px;
-	margin-right: 5px;
-	min-height: 30px;
+export const StyledBox = styled(Box)`
+	background-color: #f4cfb7;
+	min-height: 65vh;
 	padding: 10px;
 	border-radius: 5px;
-	background-color: #df9a5f;
 	box-shadow: 0px 2px 1px -1px rgb(0 0 0 / 20%), 0px 1px 1px 0px rgb(0 0 0 / 14%),
 		0px 1px 3px 0px rgb(0 0 0 / 12%);
 `;
