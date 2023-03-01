c18d7482954361ffd904d55dabc58934daab9b52
Lorna McK <lmckinley@foundationmedicine.com>
2023-03-01 16:33:17 +0000

    styling

--- ui/src/pages/boards/components/BoardItem/styled.tsx
+++ ui/src/pages/boards/components/BoardItem/styled.tsx
@@ -1,12 +1,13 @@
 import styled from 'styled-components';
-import { Box } from '@mui/material';
+import { Grid } from '@mui/material';
 
-export const BoardBox = styled(Box)`
+export const BoardBox = styled(Grid)`
 	margin-top: 20px;
 	margin-left: 5px;
 	margin-right: 5px;
-	min-height: 30px;
-	padding: 10px;
+	padding: 5px;
+	padding-left: 10px;
+	min-height: 66px;
 	border-radius: 5px;
 	box-shadow: 0px 2px 1px -1px rgb(0 0 0 / 20%), 0px 1px 1px 0px rgb(0 0 0 / 14%),
 		0px 1px 3px 0px rgb(0 0 0 / 12%);
