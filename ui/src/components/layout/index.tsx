d476b448bca50894541a451ad0be848aede8c0ce
Lorna McK <lmckinley@foundationmedicine.com>
2023-05-01 14:39:36 +0100

    remove nav

--- ui/src/components/layout/index.tsx
+++ ui/src/components/layout/index.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 const Layout: React.FC<Props> = ({ children }) => (
 	<>
-		<TempNav>nav</TempNav>
+		{/* <TempNav>nav</TempNav> */}
 		<LayoutContainer>
 			{/* <Navigation /> */}
 			{/* <PageContainer> */}
