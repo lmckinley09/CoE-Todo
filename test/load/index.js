a9e85b6e4c8c482cca7a65f9d7dd235e8d2e1118
Lorna McK <lmckinley@foundationmedicine.com>
2023-05-09 10:59:14 +0100

    verify server load

--- test/load/index.js
+++ test/load/index.js
@@ -1,16 +1,18 @@
 import http from "k6/http";
 import { check, sleep } from "k6";
 
-export const options = {
-  stages: [
-    { duration: "30s", target: 20 },
-    { duration: "1m30s", target: 10 },
-    { duration: "20s", target: 0 },
-  ],
-};
+// export const options = {
+//   stages: [
+//     { duration: "30s", target: 20 },
+//     { duration: "1m30s", target: 10 },
+//     { duration: "20s", target: 0 },
+//   ],
+// };
 
 export default function () {
-  const res = http.get("https://test.k6.io/");
+  const res = http.post("http://localhost:3001/authenticate", {
+    email: "admin@email.com",
+    password: "password1!",
+  });
   check(res, { "status was 200": (r) => r.status == 200 });
-  sleep(1);
 }
